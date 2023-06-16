882684070aeeb2a99a4d7324ac95bcfe55e9eaa1
Duxcoder <iinegve_25@mail.ru>
2023-06-16 21:09:14 +0300

    feat: create footer component

--- rs-css/src/components/view/footer/footer.ts
+++ rs-css/src/components/view/footer/footer.ts
@@ -1,5 +1,12 @@
+import NodeCreator from "../../../util/nodeCreator/nodeCreator";
+
 class Footer {
 
+  public createFooterNode(): HTMLElement {
+    const node: NodeCreator = new NodeCreator();
+    const footer: HTMLElement = node.createNode('footer', 'flex justify-center items-center col-span-2 bg-stone-800 text-white p-10'.split(' '), 'footer');
+    return footer;
+  }
 }
 
 export default Footer;
