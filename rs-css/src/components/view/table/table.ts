4138c2ed5ef9a6e047114df3fa0b03d9f81b125d
Duxcoder <iinegve_25@mail.ru>
2023-06-16 21:07:20 +0300

    feat: create table component

--- rs-css/src/components/view/table/table.ts
+++ rs-css/src/components/view/table/table.ts
@@ -1,4 +1,84 @@
+import NodeCreator from "../../../util/nodeCreator/nodeCreator";
+import './style.scss'
 class Table {
+
+  public createTitleTableNode(): HTMLElement {
+    const node: NodeCreator = new NodeCreator();
+    const title: HTMLElement = node.createNode('section', 'relative isolate px-6 pt-14 lg:px-8'.split(' '));
+    title.innerHTML = `
+      <div
+        class="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80"
+        aria-hidden="true"
+      >
+        <div
+          class="relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]"
+          style="
+            clip-path: polygon(
+              74.1% 44.1%,
+              100% 61.6%,
+              97.5% 26.9%,
+              85.5% 0.1%,
+              80.7% 2%,
+              72.5% 32.5%,
+              60.2% 62.4%,
+              52.4% 68.1%,
+              47.5% 58.3%,
+              45.2% 34.5%,
+              27.5% 76.7%,
+              0.1% 64.9%,
+              17.9% 100%,
+              27.6% 76.8%,
+              76.1% 97.7%,
+              74.1% 44.1%
+            );
+          "
+        ></div>
+      </div>
+      <div class="mx-auto max-w-2xl py-10 sm:py-16 lg:py-20">
+        <div class="text-center">
+          <h1
+            class="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl"
+          >
+            Select the plates
+          </h1>
+        </div>
+      </div>
+      <div
+        class="absolute inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-30rem)]"
+        aria-hidden="true"
+      >
+        <div
+          class="relative left-[calc(50%+3rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%+36rem)] sm:w-[72.1875rem]"
+          style="
+            clip-path: polygon(
+              74.1% 44.1%,
+              100% 61.6%,
+              97.5% 26.9%,
+              85.5% 0.1%,
+              80.7% 2%,
+              72.5% 32.5%,
+              60.2% 62.4%,
+              52.4% 68.1%,
+              47.5% 58.3%,
+              45.2% 34.5%,
+              27.5% 76.7%,
+              0.1% 64.9%,
+              17.9% 100%,
+              27.6% 76.8%,
+              76.1% 97.7%,
+              74.1% 44.1%
+            );
+          "
+        ></div>
+      </div>`
+    return title;
+  }
+
+  public createTableNode(): HTMLElement {
+    const node: NodeCreator = new NodeCreator();
+    const table: HTMLElement = node.createNode('section', 'transform-table flex relative min-w-[200px] h-[120px] w-fit p-8 mb-10 justify-center my-0 mx-auto bg-orange-950 z-1'.split(' '));
+    return table;
+  }
 
 }
 
