7d025b505c0fbe33959a09f522545a20ea613cf3
Duxcoder <iinegve_25@mail.ru>
2023-06-16 21:08:27 +0300

    feat: create codeArea component

--- rs-css/src/components/view/codeArea/codeArea.ts
+++ rs-css/src/components/view/codeArea/codeArea.ts
@@ -1,5 +1,20 @@
+import NodeCreator from "../../../util/nodeCreator/nodeCreator";
+import Panel from "./panel/panel";
 class CodeArea {
-
+  public cssEditor: Panel;
+  public htmlViewer: Panel;
+  constructor() {
+    this.cssEditor = new Panel();
+    this.htmlViewer = new Panel('dark');
+  }
+  public createCodeAreaNode(): HTMLElement {
+    const cssEditor = this.cssEditor.createPanelNode();
+    const htmlViewer = this.htmlViewer.createPanelNode();
+    const node: NodeCreator = new NodeCreator();
+    const codeArea: HTMLElement = node.createNode('section', 'flex bg-stone-300 flex-col xl:flex-row'.split(' '));
+    codeArea.append(cssEditor, htmlViewer);
+    return codeArea;
+  }
 }
 
 export default CodeArea;
---
+++ rs-css/src/components/view/codeArea/panel/panel.ts
@@ -0,0 +1,47 @@
+import NodeCreator from '../../../../util/nodeCreator/nodeCreator';
+
+class Panel {
+  constructor (public mode: 'light' | 'dark' = 'light') {
+    this.mode = mode;
+  }
+  public createPanelNode(): HTMLElement {
+    const node = new NodeCreator();
+    const panel = node.createNode('div', 'flex w-full flex-col'.split(' '));
+    panel.innerHTML = `
+    <div class="flex justify-between items-center px-8 h-10 bg-blue-100 w-full">
+      <span class ="text-white">CSS Editor</span>
+      <span class ="text-gray-500">style.css</span>
+    </div>
+    <div class ="flex justify-center w-full min-h-[300px]">
+      <div class="flex justify-end w-0.5/12 bg-stone-100">
+        <ul class="flex items-end px-4 flex-col">
+          <li>1</li>
+          <li>2</li>
+          <li>3</li>
+          <li>4</li>
+          <li>5</li>
+          <li>6</li>
+          <li>7</li>
+          <li>8</li>
+          <li>9</li>
+          <li>10</li>
+          <li>11</li>
+          <li>12</li>
+          <li>13</li>
+          <li>14</li>
+          <li>15</li>
+        </ul>
+      </div>
+      <div class="flex w-11/12 bg-white text-gray-900">
+       <pre>
+          &lt;hello&gt;
+            <bra>d</bra>
+          </helo>
+        </pre>
+      </div>
+    </div>`
+    return panel;
+  }
+}
+
+export default Panel;
