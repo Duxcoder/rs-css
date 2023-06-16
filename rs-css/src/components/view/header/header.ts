0227d39fadbdba76fcc0b657f0c2ea98cf46dcca
Duxcoder <iinegve_25@mail.ru>
2023-06-16 19:54:52 +0300

    feat: create util nodeCreator and  header view component

--- rs-css/src/components/view/header/header.ts
+++ rs-css/src/components/view/header/header.ts
@@ -1,10 +1,33 @@
 import NodeCreator from "../../../util/nodeCreator/nodeCreator";
+import logo from "./img/logo.png";
 class Header {
 
   public createHeaderNode(): HTMLElement {
-    const node = new NodeCreator();
-    const header = node.createNode('header', ['header']);
-    header.textContent = 'This is header test';
+    const node: NodeCreator = new NodeCreator();
+    const header: HTMLElement = node.createNode('header', 'absolute inset-x-0 top-0 z-50'.split(' '));
+    header.innerHTML = `
+      <nav
+        class="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8"
+      >
+        <div class="flex lg:flex-1">
+          <a href="#" class="-m-1.5 p-1.5">
+            <span class="sr-only">Your Company</span>
+            <img
+              class="h-12 w-auto"
+              src=${logo}
+              alt=""
+            />
+          </a>
+        </div>
+        <div class="flex gap-x-12">
+          <a href="https://rs.school/" target="_blink" class="text-sm font-semibold leading-6 text-gray-900"
+            >RS School</a
+          >
+          <a href="https://github.com/Duxcoder" target="_blink" class="text-sm font-semibold leading-6 text-gray-900"
+            >Duxcoder</a
+          >
+        </div>
+      </nav>`
     return header;
   }
 }
--- rs-css/src/util/nodeCreator/nodeCreator.ts
+++ rs-css/src/util/nodeCreator/nodeCreator.ts
@@ -1,9 +1,10 @@
 class NodeCreator {
   public node: HTMLElement | undefined;
 
-  public createNode(elem: string, classes: string[]): HTMLElement {
+  public createNode(elem: string, classes: string[], textContent: string | null = null): HTMLElement {
     const node: HTMLElement = document.createElement(elem);
     node.classList.add(...classes);
+    if (textContent) node.textContent = textContent
     this.node = node;
     return node;
   }
